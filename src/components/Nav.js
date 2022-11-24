cebf0383717e3a4d2aff310988da5ab49af9a3c8
Vladimir <DistoRRRtion@gmail.com>
2022-11-24 11:01:04 +0200

    10. Our Work

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,13 @@ const Nav = () => {
       <h1><a id="logo" href="#">Capture</a></h1>
       <ul>
         <li>
-          <a href="#">1. About Us</a>
+          <a href="/">1. About Us</a>
         </li>
         <li>
-          <a href="#">2. Our work</a>
+          <a href="work">2. Our work</a>
         </li>
         <li>
-          <a href="#">3. Contact Us</a>
+          <a href="contact">3. Contact Us</a>
         </li>
       </ul>
     </StyledNav>
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,16 +1,62 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom'
+// images
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
+
+
 
 const OurWork = () => {
   return (
-    <OurWorkCont>
-      <h1>OurWork</h1>
-    </OurWorkCont>
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt='athlete'></img>
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt='the racer'></img>
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt='good times'></img>
+        </Link>
+      </Movie>
+    </Work>
   )
 }
 
-const OurWorkCont = styled.div`
-  background-color: orange;
-  height: 90vh;
+const Work = styled.div`
+  min-height: 100vh;
+  color: white;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
 `;
 
 export default OurWork;
