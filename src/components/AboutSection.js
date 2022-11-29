64eb587a01a5800922b65a66a7e4c41540b7f30d
Vladimir <DistoRRRtion@gmail.com>
2022-11-29 12:12:46 +0200

    14. About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,32 +2,33 @@ import home1 from '../img/home1.png';
 import { About, Description, Image, Hide } from '../styles';
 // Framer Motion
 import { motion } from 'framer-motion';
-
+import { titleAnim, fade, photoAnimation } from '../animation';
+ 
 const AboutSection = () => {
 	return ( 
 		<About>
 			<Description>
 				<motion.div>
 					<Hide>
-						<motion.h2>We work to make</motion.h2>
+						<motion.h2 variants={titleAnim}>We work to make</motion.h2>
 					</Hide>
 					<Hide>
-						<motion.h2>
+						<motion.h2 variants={titleAnim}>
 							your <span>dreams</span> come
-						</motion.h2>
+						</motion.h2 >
 					</Hide>
 					<Hide>
-						<motion.h2>true.</motion.h2>
+						<motion.h2 variants={titleAnim}>true.</motion.h2>
 					</Hide>
-					<p>
+					<motion.p variants={fade}>
 						Contact us for any photography or videography ideas that you have.
 						We have professionals with amazing skills.
-					</p>
-					<button>Contact Us</button>
+					</motion.p>
+					<motion.button variants={fade}>Contact Us</motion.button>
 				</motion.div>
 			</Description>
 			<Image>
-				<img src={home1} alt='gay with a camera' />
+				<motion.img variants={photoAnimation} src={home1} alt='gay with a camera' />
 			</Image>
 		</About >
 	);
