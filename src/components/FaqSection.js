112904de98b61f0ea0f48b1dbadf822324202b7f
Vladimir <DistoRRRtion@gmail.com>
2022-11-29 19:47:14 +0200

    17. Reusable components

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,5 +1,7 @@
+// import { useState } from "react";
 import styled from "styled-components";
-import { About } from "../styles"
+import { About } from "../styles";
+import Toggle from './Toggle';
 
 const FaqSection = () => {
   return (
@@ -7,8 +9,26 @@ const FaqSection = () => {
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
+
+      <Toggle>
+        <div className='question'>
+          <h4>How do I Start?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Necessitatibus, ipsam.
+            </p>
+          </div>
+          <div className="faq-line"></div>
+        </div>
+      </Toggle>
+
+
+    
+
       <div className='question'>
-        <h4>How do I Start?</h4>
+        <h4>Daily Schedule</h4>
         <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
@@ -18,17 +38,7 @@ const FaqSection = () => {
         </div>
         <div className="faq-line"></div>
       </div>
-      <div className='question'>
-        <h4>How do I Start?</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Necessitatibus, ipsam.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+
       <div className='question'>
         <h4>Different Payment Methods</h4>
         <div className='answer'>
@@ -40,8 +50,9 @@ const FaqSection = () => {
         </div>
         <div className="faq-line"></div>
       </div>
+
       <div className='question'>
-        <h4>What product do you offer.</h4>
+        <h4>What product do you offer</h4>
         <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
@@ -49,6 +60,7 @@ const FaqSection = () => {
             Necessitatibus, ipsam.
           </p>
         </div>
+
       </div>
     </FAQ>
   );
